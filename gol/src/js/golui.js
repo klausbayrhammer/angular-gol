fc647b15b5c429bbfcb160f4cffe9814670daf0e
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2012-09-22 15:55:49 +0200

    backend finished

--- gol/src/js/golui.js
+++ gol/src/js/golui.js
@@ -1,17 +1,20 @@
 var golApp = angular.module('gol', []);
 
-golApp.controller('golUiCtrl', function golUiCtrl($scope) {
+golApp.controller('golUiCtrl', function ($scope)
+{
 	$scope.cellsAlive = 15;
 
-	function initRows()
+	$scope.rows = emptyBoard();
+
+	function emptyBoard()
 	{
-		maxRows = 20;
-		maxCols = 20;
+		maxRows = 15;
+		maxCols = 15;
 		rows = [];
-		for(i=0; i<maxRows; i++)
+		for (i = 0; i < maxRows; i++)
 		{
-			row = []
-			for(j=0; j<maxCols; j++)
+			row = [];
+			for (j = 0; j < maxCols; j++)
 			{
 				row.push({alive: false})
 			}
@@ -20,10 +23,51 @@ golApp.controller('golUiCtrl', function golUiCtrl($scope) {
 		return rows;
 	}
 
-	$scope.rows = initRows();
-
 	$scope.switchState = function (col)
 	{
 		col.alive = !col.alive;
 	};
+
+	$scope.iterate = function()
+	{
+		newBoard = emptyBoard();
+		iterateBoard(function(col, i, j)
+		{
+			var nrOfNeighbours = getNrOfAliveNeighbours(i, j);
+			var isAlive = nextState(col.alive, nrOfNeighbours);
+			newBoard[i][j] = {alive: isAlive};
+		});
+		$scope.rows = newBoard;
+	};
+
+	function getNrOfAliveNeighbours(cellX, cellY)
+	{
+		nrOfNeighbours = 0;
+		iterateBoard(function(col, checkX, checkY)
+		{
+			distanceX = Math.abs(cellX - checkX)
+			distanceY = Math.abs(cellY - checkY);
+			if (Math.max(distanceX, distanceY) == 1 && $scope.rows[checkX][checkY].alive)
+			{
+				nrOfNeighbours++;
+			}
+		});
+		return nrOfNeighbours;
+	}
+
+	function nextState(alive, nrOfNeighbours)
+	{
+		return (alive && nrOfNeighbours == 2) || nrOfNeighbours == 3;
+	}
+
+	function iterateBoard(closure)
+	{
+		angular.forEach($scope.rows, function(row, i)
+		{
+			angular.forEach(row, function(col, j)
+			{
+				closure(col, i, j);
+			});
+		});
+	}
 });
